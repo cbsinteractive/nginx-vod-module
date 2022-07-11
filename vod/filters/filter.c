c08cdb8f07759c503d43679691c0a70ac5e86a00
Joseph Chung <joseph.chung@cbsinteractive.com>
2022-07-11 13:54:04 -0700

    asdf

--- vod/filters/filter.c
+++ vod/filters/filter.c
@@ -300,7 +300,7 @@ filter_init_filtered_clips(
 			vod_memzero(track_count, sizeof(track_count));
 			filter_get_clip_track_count(*cur_clip, track_count);
 
-			printf("\n\n\n***%s\n\n\n***", (u_char*)media_set->sources_head->uri);
+			printf("\n\n\n***%s\n\n\n***", (u_char)media_set->sources_head->uri);
 
 			if (!media_clip_is_source(cur_clip[0]->type) && track_count[MEDIA_TYPE_AUDIO] > 1)
 			{
